5de6f2161a1a874171d423460f24ecc753f671e0
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-06-18 14:26:17 +0800

    fix: rename database to databases

--- scripts/extract-databases.sh
+++ scripts/extract-databases.sh
@@ -3,7 +3,7 @@
 set -e
 
 wd=$(pwd)
-database_dist_dir=$wd/dist/database
+database_dist_dir=$wd/dist/databases
 
 mkdir -p "$database_dist_dir"
-cp modules/database/dat/*.dat "$database_dist_dir"
+cp modules/databases/dat/*.dat "$database_dist_dir"
