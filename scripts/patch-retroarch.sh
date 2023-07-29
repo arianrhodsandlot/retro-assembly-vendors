51008f4ce085f0ec455d84f3f5a8460686efb2ee
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-07-29 14:03:13 +0800

    use git patch to apply updates to modules

--- scripts/patch-retroarch.sh
+++ scripts/patch-retroarch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-file_path="modules/retroarch/tasks/task_save.c"
-line_number=65
-strings_to_append=' || defined(EMSCRIPTEN)'
-temp_file=$(mktemp)
-awk -v num="$line_number" -v str="$strings_to_append" 'NR == num { $0 = $0 str } 1' "$file_path" > "$temp_file"
-mv "$temp_file" "$file_path"
+set -e
+
+cd modules/retroarch
+git checkout -f
+git apply ../../patches/retroarch/task_save.c.patch
