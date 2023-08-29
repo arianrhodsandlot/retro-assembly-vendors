14f27a9a58c8ad9fdf37d8dd5459466ea6d41f12
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-08-29 19:12:24 +0800

    feat: add patch to genesis-plus-gx

--- scripts/patch-modules.sh
+++ scripts/patch-modules.sh
@@ -3,8 +3,13 @@
 set -e
 
 wd=$(pwd)
-retroarch_dir=$wd/modules/retroarch
 
+retroarch_dir=$wd/modules/retroarch
 cd "$retroarch_dir"
 git checkout -f
 git apply "$wd/patches/retroarch.patch"
+
+genesis_plus_gx_dir=$wd/modules/cores/Genesis-Plus-GX
+cd "$genesis_plus_gx_dir"
+git checkout -f
+git apply "$wd/patches/genesis-plus-gx.patch"
